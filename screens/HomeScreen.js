59a2c767b9eb9a357ab7db4cab93f92f07d5ec9e
josuncom <euny_sky11@naver.com>
2022-04-23 21:57:50 +0900

    FirebaseDataReadDone

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,23 @@
 import * as React from 'react';
-import { View, Text,TouchableOpacity } from 'react-native';
+import { useState } from 'react';
+import { View, Text,TouchableOpacity, Button } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 export default function HomeScreen({navigation}) {
+
+    const [user, setUser] = useState();
+    const userCollection = firestore().collection('user');
+
+    const _callApi = async() => {
+        try{
+            const data = await userCollection.get();
+            setUser(data.docs.map(doc => ({...doc.data(), id : doc.id})));
+            console.log(user[0].name);
+        } catch(e){
+            console.log(e.message);
+        }
+    }
+
     return (
         <View style={{flex : 1, alignItems:'center', justifyContent:'center' }}>
             <TouchableOpacity>
@@ -10,6 +26,10 @@ export default function HomeScreen({navigation}) {
                 style={{fontSize:30, fontWeight:'bold'}}>Home Screen
                 </Text>
             </TouchableOpacity>
+            <Button title="데이터 읽기" onPress={_callApi}/>
+            {user?.map((row, idx) => {
+                return<Text>{row.name}</Text>
+            })}
         </View>
     );
 }
